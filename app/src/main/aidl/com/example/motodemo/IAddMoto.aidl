ac515e400055d9ab1645baf765de9ccf46bf5098
Abdul <abdul.tanveer@gmail.com>
2024-05-08 10:03:55 +0800

    Initial commit

---
+++ app/src/main/aidl/com/example/motodemo/IAddMoto.aidl
@@ -0,0 +1,9 @@
+// IAddMoto.aidl
+package com.example.motodemo;
+
+// Declare any non-default types here with import statements
+
+interface IAddMoto {
+    int add(int a,int b);
+
+}
---
+++ app/src/main/java/com/example/motoclient/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.motoclient
+
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.IBinder
+import android.os.Messenger
+import android.util.Log
+import com.example.motoclient.databinding.ActivityMainBinding
+import com.example.motodemo.IAddMoto
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private var mService: IAddMoto? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       // setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnAdd.setOnClickListener {
+            bindMyService()
+        }
+    }
+
+    private fun bindMyService() {
+        val intent = Intent("moto.add.service")
+        val pack = IAddMoto::class.java.`package`
+        intent.setPackage(pack.name)
+        bindService(intent,connection, BIND_AUTO_CREATE)
+    }
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(p0: ComponentName?, aidlBinder: IBinder?) {
+            Log.i("clientActivity", "client activity  connected to service")
+            mService = IAddMoto.Stub.asInterface(aidlBinder)
+            var sum = mService?.add(30, 40)
+            binding.tvSum.text = ""+sum
+
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            TODO("Not yet implemented")
+        }
+    }
+    }
